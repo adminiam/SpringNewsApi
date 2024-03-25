5d2bd76d18d68168395b6afd948b8a98b7a8cb55
vladimirtodoshchak <vladimirtodoshchak@gmail.com>
2024-03-25 13:05:36 +0200

    ht

---
+++ EndService/src/main/java/com/example/endservice/ExceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.example.endservice.ExceptionHandler;
+
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception e) {
+        return new ErrorResponse("An error occurred", e.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleResourceNotFoundException(ResourceNotFoundException e) {
+        return new ErrorResponse("Resource not found", e.getMessage());
+    }
+}
+
+class ErrorResponse {
+    private String error;
+    private String message;
+
+    public ErrorResponse(String error, String message) {
+        this.error = error;
+        this.message = message;
+    }
+}
